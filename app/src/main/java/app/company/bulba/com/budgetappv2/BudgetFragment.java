30e90f36e463dd05d5af1d1695b30f6ced2852e4
Zachary\Zachary <bulbasarous@gmail.com>
2018-11-23 22:49:01 +0800

    add fragment.java and fragment.xml for budget

---
+++ app/src/main/java/app/company/bulba/com/budgetappv2/BudgetFragment.java
@@ -0,0 +1,48 @@
+package app.company.bulba.com.budgetappv2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * Created by Zachary on 23/11/2018.
+ */
+
+public class BudgetFragment extends Fragment {
+
+    private EditText categoryET;
+    private EditText limitET;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup parent, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_budget, parent, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        categoryET = getView().findViewById(R.id.category_input_edit_text);
+        limitET = getView().findViewById(R.id.limit_edit_text);
+
+        Button button = getView().findViewById(R.id.button_budget_submit);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String category = categoryET.getText().toString();
+                String limitString = limitET.getText().toString();
+                int limitInt = Integer.parseInt(limitString);
+
+                Log.v("TAG: BUDGETFRAGMENT", "Category: " + category);
+                Log.v("TAG: BUDGETFRAGMENT", "Limit: " + limitInt);
+            }
+        });
+    }
+}
