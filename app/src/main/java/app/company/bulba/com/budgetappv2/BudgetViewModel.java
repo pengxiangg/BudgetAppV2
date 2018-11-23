11dd0af585986a7cb0788dc40b83edd9158a399e
Zachary\Zachary <bulbasarous@gmail.com>
2018-11-23 10:40:15 +0800

    add budget into receiptRepository and create budgetViewModel

---
+++ app/src/main/java/app/company/bulba/com/budgetappv2/BudgetViewModel.java
@@ -0,0 +1,28 @@
+package app.company.bulba.com.budgetappv2;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+
+import java.util.List;
+
+/**
+ * Created by Zachary on 23/11/2018.
+ */
+
+public class BudgetViewModel extends AndroidViewModel {
+
+    private ReceiptRepository mRepository;
+    private LiveData<List<Budget>> mAllBudgets;
+
+    public BudgetViewModel (@NonNull Application application) {
+        super(application);
+        mRepository = new ReceiptRepository(application);
+        mAllBudgets = mRepository.getAllBudgets();
+    }
+
+    LiveData<List<Budget>> getAllBudgets() {return mAllBudgets; }
+
+    public void insert(Budget budget) {mRepository.insert(budget);}
+}
--- app/src/main/java/app/company/bulba/com/budgetappv2/ReceiptRepository.java
+++ app/src/main/java/app/company/bulba/com/budgetappv2/ReceiptRepository.java
@@ -13,17 +13,23 @@ import java.util.List;
 
 public class ReceiptRepository {
     private ReceiptDao mReceiptDao;
+    private BudgetDao mBudgetDao;
     private LiveData<List<Receipt>> mAllReceipts;
+    private LiveData<List<Budget>> mAllBudgets;
 
     ReceiptRepository(Application application) {
         ReceiptRoomDatabase db = ReceiptRoomDatabase.getDatabase(application);
         mReceiptDao = db.receiptDao();
+        mBudgetDao = db.budgetDao();
         mAllReceipts = mReceiptDao.getAllReceipts();
+        mAllBudgets = mBudgetDao.getAllBudgets();
     }
 
     LiveData<List<Receipt>> getAllReceipts() {
         return mAllReceipts;
     }
+
+    LiveData<List<Budget>> getAllBudgets() { return mAllBudgets; }
 
     LiveData<Integer> getTotalCost() {
         return mReceiptDao.getTotalCost();
@@ -32,6 +38,8 @@ public class ReceiptRepository {
     public void insert(Receipt receipt) {
         new insertAsyncTask(mReceiptDao).execute(receipt);
     }
+
+    public void insert(Budget budget) { new insertBudgetAsyncTask(mBudgetDao).execute(budget); }
 
     private static class insertAsyncTask extends AsyncTask<Receipt, Void, Void> {
 
@@ -44,6 +52,18 @@ public class ReceiptRepository {
         @Override
         protected Void doInBackground(Receipt... receipts) {
             mAsyncTaskDao.insert(receipts[0]);
+            return null;
+        }
+    }
+    private static class insertBudgetAsyncTask extends AsyncTask<Budget, Void, Void> {
+
+        private BudgetDao mBudgetAsyncTaskDao;
+
+        insertBudgetAsyncTask(BudgetDao dao) {mBudgetAsyncTaskDao = dao;}
+
+        @Override
+        protected Void doInBackground(Budget... budgets) {
+            mBudgetAsyncTaskDao.insert(budgets[0]);
             return null;
         }
     }
