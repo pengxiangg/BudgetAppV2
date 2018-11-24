0be7db2c9ed7901e1b033c7b05244a5abf703f44
Zachary\Zachary <bulbasarous@gmail.com>
2018-11-24 10:44:38 +0800

    add basic navigation drawer. still need to add icons, back/up input

--- app/src/main/java/app/company/bulba/com/budgetappv2/MainActivity.java
+++ app/src/main/java/app/company/bulba/com/budgetappv2/MainActivity.java
@@ -1,11 +1,23 @@
 package app.company.bulba.com.budgetappv2;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawer;
+    private Toolbar toolbar;
+    private NavigationView nvDrawer;
 
 
     @Override
@@ -13,11 +25,72 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        nvDrawer = (NavigationView) findViewById(R.id.nvView);
+        setupDrawerContent(nvDrawer);
+
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
         ReceiptFragment fragment = new ReceiptFragment();
         fragmentTransaction.add(R.id.frag_container, fragment);
         fragmentTransaction.commit();
+        nvDrawer.getMenu().getItem(0).setChecked(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawer.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                selectDrawerItem(menuItem);
+                return true;
+            }
+        });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        Fragment fragment = null;
+        Class fragmentClass;
+        switch(menuItem.getItemId()) {
+            case R.id.nav_receipt_fragment:
+                fragmentClass = ReceiptFragment.class;
+                break;
+            case R.id.nav_budget_fragment:
+                fragmentClass = BudgetFragment.class;
+                break;
+            default:
+                fragmentClass = ReceiptFragment.class;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.frag_container, fragment).commit();
+
+        menuItem.setChecked(true);
+        if(menuItem.getItemId() != 0) {
+            setTitle(menuItem.getTitle());
+        }
+        mDrawer.closeDrawers();
     }
 }
